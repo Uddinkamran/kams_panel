29952d3074700cba603111de8414aeb7a2e95bd0
Kamran <kuddin@nyc.yearup.org>
2019-01-08 16:42:05 -0500

    added homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'welcome#index'
   # get 'posts/index'
   # get 'posts/edit'
   # get 'posts/update'
   # get 'posts/destroy'
 
-  root to: 'students#index'
+  # root to: 'students#index'
   # post '/posts' => 'posts#create'
   # get '/post/:id' => 'posts#destroy'
   # put '/post/:id' => 'posts#update'
