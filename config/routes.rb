0e588449fa2cd5f1b3052a0d638070e9b87f39e0
Kamran <kuddin@nyc.yearup.org>
2019-01-10 10:56:19 -0500

    added cohort

--- config/routes.rb
+++ config/routes.rb
@@ -22,5 +22,6 @@ Rails.application.routes.draw do
   resources :students
   resources :instructors
   resources :courses
+  resources :cohorts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_10_143358) do
+ActiveRecord::Schema.define(version: 2019_01_10_154328) do
+
+  create_table "cohorts", force: :cascade do |t|
+    t.string "name"
+    t.string "start_date"
+    t.string "end_date"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string "course_name"
